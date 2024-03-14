d1eb90afcd28693c75ae075918b3d1ffd798b045
superNWHG <geesinknico@gmail.com>
2024-03-14 14:06:11 +0100

    Added a json test script

---
+++ jsontest.py
@@ -0,0 +1,14 @@
+import json
+
+Opened = {
+    "Servo1": True,
+    "Servo2": False,
+}
+with open("data.json", "w", encoding="utf8") as file_handle:
+    json.dump(Opened, file_handle, indent=4)
+
+with open("data.json", "r", encoding="utf8") as file_object:
+    OpenedData = json.load(file_object)
+    print(OpenedData)
+
+print(OpenedData["Servo1"])
